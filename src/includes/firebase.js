63d01708dcc7b0504197be8cb2194fc2a82d0daa
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-18 23:38:49 +0200

    File upload (with progress)

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBU65huvXTqK2PGEIv4jniFlvgTEt39uXw",
@@ -15,11 +16,15 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
 
 export {
   auth,
   db,
-  usersCollection
+  storage,
+  usersCollection,
+  songsCollection
 }
