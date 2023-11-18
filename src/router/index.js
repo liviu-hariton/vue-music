aca19719e08767671de5dfa8a916ba26de3af0c5
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-18 20:29:08 +0200

    Routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,69 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import useUserStore from "@/stores/user";
+
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Manage from "@/views/Manage.vue";
+import NotFound from "@/views/404.vue";
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home,
+
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'manage',
+    /*alias: '/manage', // load same thing from two different paths (instead of redirecting the user) */
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      next();
+    },
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: {
+      name: 'manage'
+    }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: NotFound
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue')
-    }
-  ]
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
+
+// guarding routes
+router.beforeEach((to, from, next) => {
+  if(!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+
+  if(store.userLoggedIn) {
+    next();
+  } else {
+    next({
+      name: 'home'
+    });
+  }
+});
 
 export default router
