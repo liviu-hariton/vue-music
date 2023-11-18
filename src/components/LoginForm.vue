1fe2456d343e0936f1f8f36e9be8fb732d910ba7
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-18 15:59:38 +0200

    User login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -85,7 +85,7 @@ export default {
             this.login_alert_variant = "bg-green-500";
             this.login_alert_msg = "Success! Your have been logged in.";
             
-            console.log(values);
+            window.location.reload();
         }
     }
 }
--- src/stores/user.js
+++ src/stores/user.js
@@ -33,6 +33,12 @@ export default defineStore("user", {
       );
 
       this.userLoggedIn = true;
+    },
+
+    async signOut() {
+      await auth.signOut();
+
+      this.userLoggedIn = false;
     }
   }
 });
