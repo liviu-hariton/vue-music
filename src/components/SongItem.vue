8c8e15aacb91f15674b5d576ab0558c313d4cd24
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-19 23:55:13 +0200

    Jump to comments

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -1,19 +1,29 @@
 <template>
-    <li
-        class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
-    >
-        <div>
-            <router-link :to="{ name: 'song', params: { id: song.docId } }" class="font-bold block text-gray-600">{{ song.modified_name }}</router-link>
-            <span class="text-gray-500 text-sm">{{ song.name }}</span>
-        </div>
-        
-        <div class="text-gray-600 text-lg">
-            <span class="comments">
-              <i class="fa fa-comments text-gray-600"></i>
-              {{ song.comment_count }}
-            </span>
-        </div>
-    </li>
+  <li
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
+  >
+    <div>
+      <router-link
+        :to="{ name: 'song', params: { id: song.docId } }"
+        class="font-bold block text-gray-600"
+        >{{ song.modified_name }}</router-link
+      >
+      <span class="text-gray-500 text-sm">{{ song.name }}</span>
+    </div>
+
+    <div class="text-gray-600 text-lg">
+      <router-link
+        :to="{ name: 'song', params: { id: song.docId }, hash: '#comments' }"
+        custom
+        v-slot="{ navigate }"
+      >
+        <span class="comments" @click="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+      </router-link>
+    </div>
+  </li>
 </template>
 
 <script>
