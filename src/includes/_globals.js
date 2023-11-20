bd4b9bb054cd547e6d362abb003e4a70ac006837
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-20 16:32:01 +0200

    Auto-register global components

--- src/includes/_globals.js
+++ src/includes/_globals.js
@@ -2,11 +2,13 @@ import _ from 'lodash'
 
 export default {
   install(app) {
+    // Globally register all `_base`-prefixed components inside `./components/base`
     const baseComponents = import.meta.glob('../components/base/*.vue', {
       eager: true
     })
 
     Object.entries(baseComponents).forEach(([path, module]) => {
+      // Get the PascalCase version of the component name
       const componentName = _.upperFirst(
         _.camelCase(
           path
@@ -16,6 +18,7 @@ export default {
         )
       )
 
+      // Register component globally
       app.component(`Base${componentName}`, module.default || module)
     })
   }
